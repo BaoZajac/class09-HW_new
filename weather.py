05ff31524756eb3cb95dfd7b3cede0c9e9067390
Basia <bao.zajac@gmail.com>
2022-03-30 22:39:23 +0200

    dodanie requirements.txt, sprawdzenie danych z std, swyszukanie daty do sprawdzenia w danych z API

--- weather.py
+++ weather.py
@@ -4,28 +4,37 @@ import requests
 import pprint
 
 
-# if len(sys.argv) == 3:  # podano datę na wejściu
-#     API_key = sys.argv[1]
-#     date_to_check = sys.argv[2]    # TODO: sprawdzić popoprawność podania daty?
-# elif len(sys.argv) == 2:  # nie podano daty na wejściu
-#     API_key = sys.argv[1]
-#     date_to_check = datetime.datetime.now().date() + datetime.timedelta(days=1)
-# else:
-#     print("Błąd - wprowadzono niepoprawne dane wejściowe z std")
+if len(sys.argv) == 3:  # podano datę na wejściu
+    API_key = sys.argv[1]
+    date_to_check = sys.argv[2]    # TODO: sprawdzić popoprawność podania daty?
+    print(date_to_check)
+elif len(sys.argv) == 2:  # nie podano daty na wejściu
+    API_key = sys.argv[1]
+    date_to_check = datetime.datetime.now().date() + datetime.timedelta(days=1)
+    print(date_to_check)
+else:
+    print("Błąd - wprowadzono niepoprawne dane wejściowe z std")
 
 
 url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
+params = {"q": "san francisco,us", "lat": "35", "lon": "139", "cnt": "10", "units": "metric or imperial"}
+headers = {"X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com", "X-RapidAPI-Key": sys.argv[1]}
 
-querystring = {"q": "san francisco,us", "lat": "35", "lon": "139", "cnt": "10", "units": "metric or imperial"}
+response = requests.get(url, headers=headers, params=params)
 
-headers = {
-	"X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
-    "X-RapidAPI-Key": sys.argv[1]
-}
+from datetime import datetime    #TODO: dlaczego nie zaciąga tu fromtimestamp z import datetime?
+pprint.pprint(response.json())
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+user_object = (response.json())
+for date in user_object["list"]:
+    if datetime.fromtimestamp(date["dt"]).date() == date_to_check:
+        print("yes!")
+        print(user_object["list"][]["pop"])
+# date = (user_object["list"][0]["dt"])
+# print(datetime.fromtimestamp(date).date())
 
-print(response.json())
+
+
 
 # NIEDZIAŁAJĄCE API ---v----------------------------------------------------------------------
 
